6dafad2ee96427644d5c27920ab8842a5dca2122
Durgarao418 <durgaraoravuri418@gmail.com>
2025-08-03 18:19:12 +0530

    Initial commit - Spring Boot Docker App

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/example/demo/MessageController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @PostMapping
+    public Message saveMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
+    }
+
+    @GetMapping
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+}
